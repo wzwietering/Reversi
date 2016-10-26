d72089f5e2e369c4aebda7e5fadd39c73feea9ce
Maaike <maaikegalama@outlook.com>
2016-10-26 13:46:56 +0200

    Added comments to tile

--- Reversi/Reversi/Components/Tile.cs
+++ Reversi/Reversi/Components/Tile.cs
@@ -4,16 +4,26 @@ using System.Windows.Forms;
 
 namespace Reversi.Components
 {
+    // A tile (not to be confused with stone, which occupies a tile). Tiles form the board.
     public class Tile : UserControl
     {
+        // We count subscribers to the paint event to be able to remove all of them again when help is turned off.
+        // this fixes a problem of green borders that keep reappearing even when the tile is not green, due to multiple 
+        // event subscriberss.
         private int PaintEventSubscribers;
 
+        // true if this stone is occupied by a player
         public bool IsOccupied { get; private set; }
 
+        // The occupying player
         public Player Occupier { get; private set; }
 
+        // The stone control that is on this tile.
         private Stone reversiStone;
 
+        // Original (black or white) background of the tile. Saves the texture when the tile is 
+        // colored green in help mode. This way the tile can easily switch back to its original color without
+        // having to create a new bitmap texture.
         public Bitmap originalBackground { get; set; }
 
         public Tile()
@@ -53,27 +63,35 @@ namespace Reversi.Components
         public void UpdateStone(Player player)
         {
             reversiStone.BackgroundImage = ImageColorizer.ColorImage(Properties.Resources.reversiStoneLQ, player.Color);
+            this.Refresh();
         }
 
+        // Turn the tile green if showHelp = true. Otherwise, revert back to original color.
         public void ToggleHelp(bool showHelp)
         {
+            // Show a nice green tile!
             if (showHelp)
             {
+                // do a nice green border
+                this.BorderStyle = BorderStyle.FixedSingle;
                 this.Paint += DrawBorder;
                 PaintEventSubscribers++;
-                this.BorderStyle = BorderStyle.FixedSingle;
+
+                // And make the tile green marble.
                 Rectangle srcRect = new Rectangle(0, 0, Settings.TileSize, Settings.TileSize);
                 Bitmap cropped = ((Bitmap)Properties.Resources.GreenMarble).Clone(srcRect, Properties.Resources.GreenMarble.PixelFormat);
                 this.BackgroundImage = cropped;
             }
+            // revert back to original tile color.
             else
             {
+                // Remove all event subscribers!
                 for (int i = 0; i < PaintEventSubscribers; i++)
                 {
                     this.Paint -= DrawBorder;
                 }
 
-                this.BorderStyle = System.Windows.Forms.BorderStyle.None;
+                this.BorderStyle = BorderStyle.None;
                 this.BackgroundImage = this.originalBackground;
                 Invalidate();
             }
@@ -88,6 +106,7 @@ namespace Reversi.Components
                 Color.MediumSeaGreen, 2, ButtonBorderStyle.Solid);
         }
 
+        // Fire the clik event. Used for the AI to 'click' on tiles programaticall
         public void ProgrammaticClick()
         {
             base.OnClick(new System.EventArgs());
